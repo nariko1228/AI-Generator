2f30aef07e25f904ac8af7e8176d4551a3f7df82
nariko1228 <nariko1228@live.com>
2024-09-25 11:14:50 +0100

    added user experience

--- src/index.js
+++ src/index.js
@@ -14,14 +14,13 @@ function generatePoem(event) {
 
   let apiKey = "8a1b54d66o1c443a9f80t9d2a4d2b05d";
   let context =
-    "You are a romantic Chinese poem expert and love to write short poems. Your missions is to generate chinese poem with english translation and separate each line with a <br/>. Must follow user instructions to generate chinese poem";
+    "You are a romantic Chinese poem expert and love to write short poems. Your missions is to generate chinese poem with english in next colum and separate each line with a <br/>. Must follow user instructions to generate chinese poem";
   let prompt = `User instructions: Generate a Chinese poem about ${instructionsInput.value}`;
 
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
-  console.log("Generating poem");
-  console.log(`Prompt:${prompt}`);
-  console.log(`Context:${context}`);
+  let poemElement = document.querySelector("#poem");
+  poemElement.innerHTML = `<div class="loading">Generating the poem about ${instructionsInput.value}...</div>`;
 
   axios.get(apiURL).then(displayPoem);
 }
